f5cab994a6dfeb7f670264f537e0c3dc5f6f2f97
Kendall2300 <kendallmartinezcarvajal4>
2020-12-16 09:26:00 -0600

    Fixed buidl.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #1/bin/bash
 
-echo'1/2 Starting the build react'
+echo'1/3 Getting dependencies...'
+npm install
+
+echo'2/3 Starting the build react...'
 npm run build
 
-echo'2/2 Packing the application'
+echo'3/3 Packing the application...'
 docker build . -t star-wars-server
 
-echo'2/2 Completed. Bye'
+echo'3/3 Completed. Bye'
